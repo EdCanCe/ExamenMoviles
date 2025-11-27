cee3813ff8843a3292731f809492330b786e64c8
Edmundo Canedo Cervantes <edcance.dev@gmail.com>
2025-11-26 18:22:47 -0600

    [ADD] Basic skeleton for swift development

---
+++ ExamenMoviles/ExamenMovilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamenMovilesApp.swift
+//  ExamenMoviles
+//
+//  Created by Edmundo Canedo Cervantes on 26/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ExamenMovilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
